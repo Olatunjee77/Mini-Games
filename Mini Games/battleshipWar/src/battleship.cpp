af85021a752e29871e62f9ae36e333f3295dee3e
Olatunjee77 <olatunjiidris50@gmail.com>
2025-09-09 19:34:33 +0100

    Added explanation and an array of sample battlefield and expanded the battlefield.

--- Mini Games/battleshipWar/src/battleship.cpp
+++ Mini Games/battleshipWar/src/battleship.cpp
@@ -7,20 +7,57 @@ int main(){
     cout << "**********************************" << endl;
     cout << "******** BATTLESHIP WAR **********" << endl;
     cout << "**********************************" << endl;
-    
-    // Battleships's Coordinates: '1' indicates presence of a ship while '0' indicates absecnce 
-    bool userPlane[][4] ={
-        {0, 0, 0, 1},
-        {1, 0, 0, 1},
-        {0, 0, 1, 0},
-        {0, 0, 1, 0}
+
+    cout << "This is a sample of the Battlefield." << 
+            endl << "The No 1-8 represent the Coordinates of Ships '0'. " << 
+            endl << "There are 16 Ships on the Battlefield, Hit your opponent's ship to gain points." << endl; 
+
+    int sampleBoard[][9] = {
+        {0, 1, 2, 3, 4, 5, 6, 7, 8},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {2, 0, 0, 0, 0, 0, 0, 0, 0},
+        {3, 0, 0, 0, 0, 0, 0, 0, 0},
+        {4, 0, 0, 0, 0, 0, 0, 0, 0},
+        {5, 0, 0, 0, 0, 0, 0, 0, 0},
+        {6, 0, 0, 0, 0, 0, 0, 0, 0},
+        {7, 0, 0, 0, 0, 0, 0, 0, 0},
+        {8, 0, 0, 0, 0, 0, 0, 0, 0}
     };
 
-    bool computerPlane[][4] ={
-        {0, 0, 0, 1},
-        {1, 0, 0, 1},
-        {0, 0, 1, 0},
-        {0, 0, 1, 0}
+    int boardRow = sizeof(sampleBoard) / sizeof(sampleBoard[0]);
+    int boardCol = sizeof(sampleBoard[0]) / sizeof(sampleBoard[0][0]);
+
+    for(int i=0; i<boardRow; i++){
+        for(int j=0; j<boardCol; j++){
+            cout << sampleBoard[i][j] << "   ";
+
+            
+        }
+        cout << endl;
+    }
+    cout << endl;
+    
+    // Battleships's Coordinates: '1' indicates presence of a ship while '0' indicates absecnce 
+    bool userPlane[][8] ={
+        {0, 1, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 1, 0, 0, 1},
+        {0, 1, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1, 0}
+    };
+
+    bool computerPlane[][8] ={
+        {1, 0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 1, 0, 1, 0, 0},
+        {1, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 1, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 1, 1, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 1}
     };
     
     srand(time(NULL));
@@ -29,9 +66,9 @@ int main(){
     int computersHit = 0;
     int computersTurn = 0;
  
-    cout << "Enter co-ordinate values between 0 and 3" << endl;
+    cout << "Enter co-ordinate values between 0 and 7" << endl;
     
-    while(usersHit < 5){
+    while(usersHit < 16 || computersHit < 16){
         // User's Turn
         int row; 
         int col;
@@ -62,8 +99,8 @@ int main(){
         cout << "------------------------------------------>>" << endl << endl;
 
         // Computer's Turn 
-        int c_row = rand() % 4; //Computer's coordinates
-        int c_col = rand() % 4;
+        int c_row = rand() % 8; //Computer's coordinates
+        int c_col = rand() % 8;
         
         cout << "--------------------------------------------" << endl;
         cout << "\t\tCOMPUTER'S TURN" << endl;
@@ -91,12 +128,12 @@ int main(){
         cout << "Computer: " << computersHit << endl;
     }
 
-    if(usersHit == 5){
+    if(usersHit == 16){
         cout << "\nYou win!" << endl;
         cout << "You hit " << usersHit << " Battleships." << endl;
         cout << "Computer hit " << computersHit << " Battleships";
     }
-    else if(computersHit == 5){
+    else if(computersHit == 16){
         cout << "\nYou lose!" << endl;
         cout << "You hit " << usersHit << " Battleships." << endl;
         cout << "Computer hit " << computersHit << " Battleships";
